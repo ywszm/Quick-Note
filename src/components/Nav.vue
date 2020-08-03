b11fe895478f81a4f32355feef8c8018bb949cd1
ywszm <aichengwan1@163.com>
2020-08-03 22:26:18 +0800

    创建 <Icon />

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="nav">
-    <router-link to="/money">记帐</router-link>
+    <router-link to="/money">
+      <Icon name="money"/>
+      记帐
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
+import Icon from '@/components/Icon.vue';
+
 export default {
-  name: 'Nav'
+  name: 'Nav',
+  component: {Icon}
 };
 </script>
 
 <style lang="scss" scoped>
-  .nav{
-    border:1px solid red;
-  }
+.nav {
+  border: 1px solid red;
+}
 </style>
