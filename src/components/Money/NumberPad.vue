ee93f772c5c3b370312874686a7e86f375c4789c
ywszm <aichengwan1@163.com>
2020-08-04 18:53:41 +0800

    收集四个组件的数据到 record

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -13,7 +13,7 @@
       <button @click="inputContent">7</button>
       <button @click="inputContent">8</button>
       <button @click="inputContent">9</button>
-      <button class="ok">OK</button>
+      <button @click="ok" class="ok">OK</button>
       <button @click="inputContent" class="zero">0</button>
       <button @click="inputContent">.</button>
     </div>
@@ -22,12 +22,12 @@
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
 
   @Component
   export default class NumberPad extends Vue {
-    output = '0';
-
+    @Prop() readonly value!: number;
+    output = this.value.toString();
 
     inputContent(event: MouseEvent) {
       const button = (event.target as HTMLButtonElement);
@@ -58,6 +58,11 @@
       this.output = '0';
     }
 
+    ok() {
+      this.$emit('update:value', this.output);
+      this.$emit('submit', this.output);
+      this.output = '0';
+    }
 
   }
 </script>
@@ -73,7 +78,7 @@
       font-family: Consolas, monospace;
       padding: 9px 16px;
       text-align: right;
-      min-height: 72px;
+      height: 72px;
     }
 
     .buttons {
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,27 +1,60 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad/>
-    <Types/>
-    <Notes/>
-    <Tags :data-source.sync="tags"/>
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
+    <Types :value.sync="record.type"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
   </Layout>
 </template>
 
-<script>
-  import NumberPad from '@/components/Money/NumberPad.vue'
-  import Types from '@/components/Money/Types.vue'
-  import Notes from '@/components/Money/Notes.vue'
-  import Tags from '@/components/Money/Tags.vue'
+<script lang="ts">
+  import Vue from 'vue';
+  import NumberPad from '@/components/Money/NumberPad.vue';
+  import Types from '@/components/Money/Types.vue';
+  import Notes from '@/components/Money/Notes.vue';
+  import Tags from '@/components/Money/Tags.vue';
+  import {Component, Watch} from 'vue-property-decorator';
 
-  export default {
-    name: 'Money',
-    components: { Tags, Notes, Types, NumberPad },
-    data() {
-      return {
-        tags: ['衣', '食', '住', '行']
-      }
+  const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+
+  type Record = {
+    tags: string[];
+    notes: string;
+    type: string;
+    amount: number;
+    createdAt?: Date;
+  };
+
+  @Component({
+    components: {Tags, Notes, Types, NumberPad},
+  })
+  export default class Money extends Vue {
+    tags = ['衣', '食', '住', '行'];
+    recordList: Record[] = recordList;
+    record: Record = {
+      tags: [], notes: '', type: '-', amount: 0
+    };
+
+    onUpdateTags(value: string[]) {
+      this.record.tags = value;
+    }
+
+    onUpdateNotes(value: string) {
+      this.record.notes = value;
+    }
+
+    saveRecord() {
+      const record2: Record = JSON.parse(JSON.stringify(this.record));
+      record2.createdAt = new Date();
+      this.recordList.push(record2);
+    }
+
+    @Watch('recordList')
+    onRecordListChange() {
+      window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
     }
   }
+
 </script>
 
 <style lang="scss">
